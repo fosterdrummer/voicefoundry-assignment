5bc494a69147f2b94487a08fa66c233e851869a9
Hunter Foster <hunterafoster@gmail.com>
2020-12-22 00:14:11 -0600

    Debug

--- tulsa-weather-app/react-app/src/clients/tulsa-weather-client.ts
+++ tulsa-weather-app/react-app/src/clients/tulsa-weather-client.ts
@@ -3,7 +3,7 @@ import config from '../apiConfig.json'
 import { WeatherResponse } from 'openweathermap-api-client'
 
 class TulsaWeatherClient{
-    apiUrl: string = config.TulsaWeatherApiDeployment.apiUrl
+    apiUrl: string = config.TulsaWeatherApiDeployment.ApiUrl
     async getCurrentWeather(): Promise<WeatherResponse>{
         return axios.get(this.apiUrl)
             .then(response => response.data as WeatherResponse);
