df1c5c148ee4e0383860329dbb60da3ce184e12b
Hunter Foster <hunterafoster@gmail.com>
2020-12-31 03:39:51 -0600

    These secrets must be kept!

--- tulsa-weather-deploy/lib/stacks/deploy.ts
+++ tulsa-weather-deploy/lib/stacks/deploy.ts
@@ -13,7 +13,7 @@ export class DeployStack extends cdk.Stack{
             githubSourceProps: {
                 owner: 'fosterdrummer',
                 repo: 'voicefoundry-assignment',
-                oauthToken: cdk.SecretValue.secretsManager('GithubToken'),
+                oauthToken: cdk.SecretValue.secretsManager('GithubPrivateToken'),
                 branch: 'develop'
             },
             apiStackProps: {
